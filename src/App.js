1255b27a48feb37b97b0911f31b2903854a7734e
samimeadad <sami.meadad@gmail.com>
2022-10-09 18:27:01 +0600

    website title changed

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ function App () {
     <div className="App">
       <header className="App-header">
         <img src={ logo } className="App-logo" alt="logo" />
-        <h1>Hi, This is Sami Meadad Choudhury, the proprietor of bit2code.com. We develop web application for our clients. This site is under development at this moment. We will come back soon after some major updates. Please stay with us..</h1>
+        <h1>Hi, This is Sami Meadad Choudhury, the proprietor of bit2code.com. We develop web application for our clients. This site is under development at this moment. We will come back soon after some major updates. Please stay with us.
+        </h1>
         <a
           className="App-link"
           href="https://www.linkedin.com/in/sami-meadad-choudhury/"
